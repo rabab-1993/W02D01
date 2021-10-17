fb630267add1b9c89e3ee29c5d662aacd357935f
rabab-1993 <Rabab.Abdulaziz.elq.js@tuwaiq.edu.sa>
2021-10-17 13:34:10 +0300

    file added

---
+++ app.js
@@ -0,0 +1,91 @@
+// 1
+function sum(num1, num2){
+     return num1+num2;
+}
+
+sum(34,60);
+
+
+// 2
+function average(num1, num2) {
+    return num1-num2;
+}
+
+
+
+// 3
+// function findFactorial(num) {
+    
+// }
+
+// 4
+function round (number) {
+    return Math.round(number);
+}
+
+// 5
+
+function toThePowerOf(base, exponent,) {
+    return Math.pow(base, exponent)
+}
+
+
+// 6
+
+function randomNumber(num) {
+    return Math.random(num)
+}
+
+
+// 7
+
+function oneOrZero(num) {
+    return Math.floor(randomNumber() * num);
+}
+
+// 8
+
+// function randomRange(number) {
+//     return Math.floor(randomNumber() + (number) + number)
+// }
+
+// 9
+
+function includeOf(string, character) {
+
+    if (string.includes(character)) {
+        
+        return true;
+    }else{
+        return false
+    }
+}
+
+
+// Array task
+
+// 1
+function addToArray(array, string) {
+    
+}
+
+// 2
+function convertToString(params) {
+    
+}
+
+// 3
+
+function updateValue(params) {
+    
+}
+
+// 4
+
+
+
+// 5
+
+// 6
+
+// 7
